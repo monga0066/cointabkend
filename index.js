8fb685be1f633e939789d8541745186a20c8bea5
monga0066 <rahulmonga8929.com>
2023-02-26 22:40:55 +0530

    dddd

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ require("dotenv").config();
 
 
 const app = express();
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send({ message: "welcome" });
